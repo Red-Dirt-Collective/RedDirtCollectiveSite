1b5d37b0924bce1d64cbc3fdf03a19df771fa953
Caleb Creed <thirdcreed@gmail.com>
2020-07-04 15:00:59 -0500

    add parrallax

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,18 @@ import dustBowl from '../DustBowl.jpg';
 
 export default () => (
   <div class="main-content" style ={{"text-align":"center"}}> 
-    <h1>RED DIRT COLLECTIVE</h1>    
+    <h1>RED DIRT COLLECTIVE</h1>  
+    <h3 style= {{"margin-top":"200px","color":"#f5f4e1", "font-size":"20px"}}> Helping one another through our crisis.</h3> 
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa nibh, rutrum at tincidunt id, dictum vitae lorem. Mauris est ipsum, malesuada a ornare eget, tincidunt sit amet eros. Proin suscipit ullamcorper turpis, vitae auctor odio pulvinar ut. Integer vitae ipsum viverra ligula mollis vestibulum id vel ex. Praesent quis quam a felis commodo rutrum. Praesent iaculis vehicula malesuada. Maecenas viverra fringilla aliquet. Fusce mattis ante sit amet magna lacinia, a vulputate nisl rutrum.
+
+Aenean tellus dui, mollis non convallis ut,</p><p> elementum nec diam. Ut porttitor est vel lorem interdum pharetra. Integer euismod, felis id semper posuere, nunc nisl aliquet mi, ac finibus tortor nunc quis leo. Pellentesque non cursus erat. Ut id varius odio, vitae rhoncus leo. Nam lectus elit, malesuada ac finibus vitae, sagittis a metus. Fusce sodales ligula viverra risus tempus blandit. Sed facilisis erat tempus tellus ullamcorper, id feugiat lectus placerat.
+
+Morbi fringilla ipsum velit, et aliquet nulla consectetur nec. Vivamus condimentum justo eros, et gravida ipsum elementum quis. Nulla facilisi. Praesent vulputate eros ligula, id tincidunt elit pulvinar nec. Aenean non fermentum mi. Etiam tincidunt, sem quis consectetur semper, mauris lacus malesuada orci, at efficitur turpis tellus blandit sem. Fusce vehicula, libero ut pulvinar viverra, sem magna ornare est, rutrum imperdiet dui ante a velit. Donec mattis dui leo, vel posuere libero pharetra ac. Curabitur at leo consectetur, aliquet nibh eget, dignissim ante. Mauris luctus sollicitudin velit ornare semper. Donec euismod risus non erat auctor, ac mattis ligula fringilla.
+
+Suspendisse euismod et est et rhoncus. Suspendisse et pellentesque libero, sit amet porttitor ex. Praesent commodo eget eros a pulvinar. Maecenas consectetur nulla et suscipit malesuada. Ut dapibus urna mi, vitae ultrices diam lacinia ut. Pellentesque fringilla ornare ipsum at faucibus. Sed ut augue velit. Aliquam ultricies varius turpis, in porta quam laoreet sed. Sed suscipit ligula lorem, sed tristique dolor fringilla non. Aenean rhoncus dolor eget imperdiet posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam eu rhoncus ante, eu volutpat odio. Maecenas nec sem eu nunc volutpat maximus. Nulla quam dolor, finibus placerat rhoncus in, accumsan id felis.
+
+Nulla in nibh vel mauris finibus elementum. Curabitur sodales, erat luctus finibus malesuada, augue nibh commodo sem, a fringilla metus eros at lacus. Nullam at varius orci, ac varius ipsum. Praesent et sapien gravida, fermentum purus quis, vehicula nunc. Nunc semper finibus bibendum. Ut sed sagittis elit, elementum lacinia elit. Nulla egestas, felis sit amet varius elementum, magna purus dignissim massa, et sodales nibh est at nunc. Ut eget ligula a nisl rutrum tempor. Phasellus ut quam felis. Mauris efficitur congue orci non cursus. Phasellus quis elit a enim ullamcorper fermentum.</p>
+<i class="more"></i>
   </div>
+ 
 )
