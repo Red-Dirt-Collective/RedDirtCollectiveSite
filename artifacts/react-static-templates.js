bcbbfe171d76faeae4b29986d00610d9b638cfb8
okaybenji <okaybenji@gmail.com>
2020-07-12 11:50:00 -0500

    remove Dynamic page

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -6,6 +6,7 @@ export default {
   '__react_static_root__/src/pages/404.js': require('__react_static_root__/src/pages/404.js').default,
 '__react_static_root__/src/pages/about.js': require('__react_static_root__/src/pages/about.js').default,
 '__react_static_root__/src/pages/index.js': require('__react_static_root__/src/pages/index.js').default,
+'__react_static_root__/src/pages/policies.js': require('__react_static_root__/src/pages/policies.js').default,
 '__react_static_root__/src/containers/Post': require('__react_static_root__/src/containers/Post').default
 }
 
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
 //
 import { Link, Router } from 'components/Router'
-import Dynamic from 'containers/Dynamic'
 
 import './app.css'
 
@@ -16,12 +15,10 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/policies">Policies</Link>
-        <Link to="/dynamic">Dynamic</Link>
       </nav>
       <div className="content">
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
-            <Dynamic path="dynamic" />
             <Routes path="*" />
           </Router>
         </React.Suspense>
