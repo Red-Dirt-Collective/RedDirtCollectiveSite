fd3169949d774a774fde6ada28d7412fe64ba9b7
jimmyfm <jimmyfm@users.noreply.github.com>
2022-04-26 09:18:16 -0500

    Dependencies cleanup

--- static.config.js
+++ static.config.js
@@ -1,22 +1,7 @@
 import path from 'path'
-import ical from 'node-ical'
-
 
 export default {
-  getRoutes: async () => {
-
-    const calendar = await ical.async.fromURL('https://calendar.google.com/calendar/ical/rdc.normanok%40gmail.com/public/basic.ics');
-
-    return [
-      {
-        path: '/calendar',
-        template: 'src/pages/calendar.js',
-        getData: async (resolved) => { 
-          console.log({calendar,resolved})
-          return {calendar} }
-      },
-    ]
-  },
+  getRoutes: async () => { return []; },
   plugins: [
     [
       'react-static-plugin-less',
